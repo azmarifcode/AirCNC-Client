85917c1f04f1b5e0f8ccc28bc8d0684d84d5024b
A. Z. M. Arif <azmarifcode@gmail.com>
2022-11-22 05:09:54 +0600

    c

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,27 +5,48 @@ import Home from '../Pages/Home'
 import Login from '../Pages/Login/Login'
 import Signup from '../Pages/Login/Signup'
 import Main from '../Layout/Main'
+import ComingSoon from '../Pages/Shared/ComingSoon'
+import Details from '../Pages/Details'
+import SearchResult from '../Pages/SearchResult'
+import PrivateRoute from './PrivateRoute'
+import Checkout from '../Pages/Checkout'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
+    {
         path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/signup',
-        element: <Signup />,
-      },
-    ],
-  },
-])
+        element: <Main />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/signup',
+                element: <Signup />,
+            },
+            {
+                path: '/coming-soon',
+                element: <ComingSoon />,
+            },
+            {
+                path: '/service-details',
+                element: <Details />,
+            },
+            {
+                path: '/search-result',
+                element: <SearchResult />,
+            },
+            {
+                path: '/checkout',
+                element: <PrivateRoute><Checkout/></PrivateRoute>,
+            },
+        ],
+    },
+]);
 
 export default router
