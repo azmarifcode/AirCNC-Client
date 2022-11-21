36cf2a81d53b800c6b3376047cc52facca43bfe7
A. Z. M. Arif <azmarifcode@gmail.com>
2022-11-21 11:43:19 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBuvUc3Jgyu85szZlSXNG_uh0yLW4ELYeM',
+    authDomain: 'email-password-auth-a5350.firebaseapp.com',
+    projectId: 'email-password-auth-a5350',
+    storageBucket: 'email-password-auth-a5350.appspot.com',
+    messagingSenderId: '1044581546191',
+    appId: '1:1044581546191:web:3b0b621668f6c438f61fba',
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
